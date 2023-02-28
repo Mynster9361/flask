6cda257bde2cba471b1a1f2019b6c74a12f2909a
-Mynster <mortenmynster@hotmail.com>
2023-02-28 21:30:31 +0100

    moved to prod

--- email_signature_lookup/app.py
+++ email_signature_lookup/app.py
@@ -12,4 +12,5 @@ def index(email):
     return response.text
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
